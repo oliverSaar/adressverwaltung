02ed19d61767d4393aa672a31e5a70bb7dd95617
Oliver Saar <oli.saar123@gmail.com>
2024-03-26 15:51:59 +0100

    began with testing

---
+++ adressverwaltung_model/src/test/java/de/dhbw/ase/model/PersonTest.java
@@ -0,0 +1,41 @@
+package de.dhbw.ase.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+    @Test
+    void testAddNewPerson(){
+        //arrange
+        //brauchen Person + Repo
+
+        //act
+        //person in repo speichern
+
+        //assert
+        //assertthat lastUpdated is null
+    }
+
+
+    @Test
+    void testUpdateLastModified(){
+        //triple a schema: arrange, act, assert
+
+        //arrange
+//        Person person = new Person();
+        //repository erstellen
+        //eintrag zu repository hinzufügen
+
+        //act
+        //person in repository speichern
+
+        //assert
+        //person in repository suchen
+        //assertThat()
+    }
+
+}
