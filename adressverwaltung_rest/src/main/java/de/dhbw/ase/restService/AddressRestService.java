c8286d1bdf3dc204e031d233dffc6a38591f6e30
Oliver Saar <oli.saar123@gmail.com>
2024-04-10 13:09:50 +0200

    added methods for view

--- adressverwaltung_rest/src/main/java/de/dhbw/ase/restService/AddressRestService.java
+++ adressverwaltung_rest/src/main/java/de/dhbw/ase/restService/AddressRestService.java
@@ -1,4 +1,18 @@
 package de.dhbw.ase.restService;
 
+import de.dhbw.ase.model.Address;
+
 public class AddressRestService {
+    
+    
+    public void getAllAddresses(){
+        
+    }
+
+    public void getAddress(int id) {
+    }
+
+    public void addAddress(Address address) {
+
+    }
 }
