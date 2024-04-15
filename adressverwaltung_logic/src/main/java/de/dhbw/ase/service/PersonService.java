900d97ef446a050015af94e9e1f7e661280ef715
Oliver Saar <oli.saar123@gmail.com>
2024-04-15 14:51:23 +0200

    PersonService mit ID Counter aktualisiert

--- adressverwaltung_logic/src/main/java/de/dhbw/ase/service/PersonService.java
+++ adressverwaltung_logic/src/main/java/de/dhbw/ase/service/PersonService.java
@@ -1,9 +1,32 @@
 package de.dhbw.ase.service;
 
+import de.dhbw.ase.model.Person;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
 public class PersonService {
 
-    protected PersonService(){
+    protected PersonService() {
 
+    }
+
+    private final static AtomicLong ID_COUNTER = new AtomicLong();
+
+    private final Map<Long, Person> persons = new HashMap<>();
+
+    public static long getNextId() {
+        return ID_COUNTER.getAndIncrement();
+    }
+
+    public Person getPerson(long id) {
+        return persons.get(id);
+    }
+
+    public void addPerson(Person person) {
+//TODO vllt eher in PersonDAO
+        persons.put((person.getId().longValue()), person);
     }
 
 
