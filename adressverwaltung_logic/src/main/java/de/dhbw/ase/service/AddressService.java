9ab0aa8b29fea0236c91d193d74b82c863de6d2c
Oliver Saar <oli.saar123@gmail.com>
2024-04-16 15:09:02 +0200

    added idCounter

--- adressverwaltung_logic/src/main/java/de/dhbw/ase/service/AddressService.java
+++ adressverwaltung_logic/src/main/java/de/dhbw/ase/service/AddressService.java
@@ -1,8 +1,16 @@
 package de.dhbw.ase.service;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class AddressService {
 
-    protected AddressService(){
+    protected AddressService() {
 
+    }
+
+    private final static AtomicLong ID_COUNTER = new AtomicLong();
+
+    public static long getNextId() {
+        return ID_COUNTER.incrementAndGet();
     }
 }
--- adressverwaltung_logic/src/main/java/de/dhbw/ase/service/PhoneNumberService.java
+++ adressverwaltung_logic/src/main/java/de/dhbw/ase/service/PhoneNumberService.java
@@ -1,8 +1,16 @@
 package de.dhbw.ase.service;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class PhoneNumberService {
 
     protected PhoneNumberService() {
     }
 
+
+    private final static AtomicLong ID_COUNTER = new AtomicLong();
+
+    public static long getNextId() {
+        return ID_COUNTER.incrementAndGet();
+    }
 }
