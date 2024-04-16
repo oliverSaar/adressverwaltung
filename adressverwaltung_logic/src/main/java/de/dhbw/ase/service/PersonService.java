ebd83c6e9a0bb583590dc8544115a0c2ba9032d8
Oliver Saar <oli.saar123@gmail.com>
2024-04-16 15:07:54 +0200

    refactoring

--- adressverwaltung_logic/src/main/java/de/dhbw/ase/service/PersonService.java
+++ adressverwaltung_logic/src/main/java/de/dhbw/ase/service/PersonService.java
@@ -2,8 +2,6 @@ package de.dhbw.ase.service;
 
 import de.dhbw.ase.model.Person;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class PersonService {
@@ -14,19 +12,17 @@ public class PersonService {
 
     private final static AtomicLong ID_COUNTER = new AtomicLong();
 
-    private final Map<Long, Person> persons = new HashMap<>();
 
     public static long getNextId() {
-        return ID_COUNTER.getAndIncrement();
+        return ID_COUNTER.incrementAndGet();
     }
 
     public Person getPerson(long id) {
-        return persons.get(id);
+        return null;
     }
 
     public void addPerson(Person person) {
-//TODO vllt eher in PersonDAO
-        persons.put((person.getId().longValue()), person);
+
     }
 
 
