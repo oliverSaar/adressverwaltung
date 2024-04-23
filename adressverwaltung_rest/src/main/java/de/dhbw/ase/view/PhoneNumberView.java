0423da9160d5ef1352f20b9f65c9eef6eaede6f4
Oliver Saar <oli.saar123@gmail.com>
2024-04-23 13:42:20 +0200

    adjusted constructor for menu navigation

--- adressverwaltung_rest/src/main/java/de/dhbw/ase/view/PhoneNumberView.java
+++ adressverwaltung_rest/src/main/java/de/dhbw/ase/view/PhoneNumberView.java
@@ -1,13 +1,22 @@
 package de.dhbw.ase.view;
 
+import de.dhbw.ase.restHelperService.AddressRestHelperService;
+import de.dhbw.ase.restHelperService.PersonRestHelperService;
 import de.dhbw.ase.restHelperService.PhoneNumberRestHelperService;
+import de.dhbw.ase.service.LoginService;
 
 public class PhoneNumberView implements View {
 
     private final PhoneNumberRestHelperService phoneNumberRestHelperService;
+    private final PersonRestHelperService personRestHelperService;
+    private final AddressRestHelperService addressRestHelperService;
+    private final LoginService loginService;
 
-    public PhoneNumberView(PhoneNumberRestHelperService phoneNumberRestHelperService) {
+    public PhoneNumberView(PhoneNumberRestHelperService phoneNumberRestHelperService, PersonRestHelperService personRestHelperService, AddressRestHelperService addressRestHelperService, LoginService loginService) {
         this.phoneNumberRestHelperService = phoneNumberRestHelperService;
+        this.personRestHelperService = personRestHelperService;
+        this.addressRestHelperService = addressRestHelperService;
+        this.loginService = loginService;
     }
 
     public void defaultView() {
