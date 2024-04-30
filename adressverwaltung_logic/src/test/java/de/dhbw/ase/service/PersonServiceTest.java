41e147d9782b69e78c4e31f4ddf53fef0d93839b
Oliver Saar <oli.saar123@gmail.com>
2024-04-30 17:55:06 +0200

    testing personservice

--- adressverwaltung_logic/src/test/java/de/dhbw/ase/service/PersonServiceTest.java
+++ adressverwaltung_logic/src/test/java/de/dhbw/ase/service/PersonServiceTest.java
@@ -1,18 +1,21 @@
 package de.dhbw.ase.service;
 
+import de.dhbw.ase.model.Address;
 import de.dhbw.ase.model.Person;
+import de.dhbw.ase.model.PhoneNumber;
 import de.dhbw.ase.service.mocks.PersonDAOMock;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PersonServiceTest {
 
+
     public PersonDAOMock personDAOMock;
+    PersonService personService;
 
     public Person toAdd;
 
@@ -20,6 +23,7 @@ class PersonServiceTest {
     void setUp() {
 
         personDAOMock = new PersonDAOMock();
+        personService = new PersonService(personDAOMock);
 
         toAdd = new Person(0, "testV", "testN", 29, 4, 1990, null, null);
 
@@ -29,62 +33,91 @@ class PersonServiceTest {
     @Test
     void getAllPersons() {
 
-
-        assertEquals(3, personDAOMock.getPersons().size());
-
+        assertEquals(3, personService.getAllPersons().size());
 
     }
 
     @Test
     void getPerson() {
 
-        Optional<Person> optionalPerson = personDAOMock.getPerson(0);
-        if (optionalPerson.isPresent()) {
+        Person person = personService.getPerson(0);
 
-            assertEquals("vorname", optionalPerson.get().getFirstName());
-            assertEquals("nachname", optionalPerson.get().getLastName());
-            assertEquals(LocalDate.of(1990, 4, 29), optionalPerson.get().getDateOfBirth());
-        }
-
-
+        assertEquals("vorname", person.getFirstName());
+        assertEquals("nachname", person.getLastName());
+        assertEquals(LocalDate.of(1990, 4, 29), person.getDateOfBirth());
     }
 
     @Test
     void addPerson() {
 
-        assertEquals(3, personDAOMock.getPersons().size());
+        assertEquals(3, personService.getAllPersons().size());
 
-        personDAOMock.insertPerson(toAdd);
+        personService.addPerson(toAdd);
 
-        assertEquals(4, personDAOMock.getPersons().size());
+        assertEquals(4, personService.getAllPersons().size());
+        assertEquals("testV", personService.getAllPersons().get(3).getFirstName());
 
     }
 
     @Test
     void updatePerson() {
+        assertEquals("vorname", personService.getPerson(0).getFirstName());
+        personService.updatePerson(toAdd, toAdd.getId());
+        assertEquals("testV", personService.getPerson(0).getFirstName());
+
+
     }
 
     @Test
     void followPerson() {
+        assertEquals(0, personService.getPerson(0).getFollowing().size());
+        Person p = personService.getPerson(0);
+        p.addFollowing(personService.getPerson(1));
+        personService.updatePerson(p, p.getId());
+        assertEquals(1, personService.getPerson(0).getFollowing().size());
     }
 
     @Test
     void unfollowPerson() {
+
+        assertEquals(1, personService.getPerson(1).getFollowing().size());
+        Person p = personService.getPerson(1);
+        p.removeFollowing(personService.getPerson(0));
+        personService.updatePerson(p, p.getId());
+        assertEquals(0, personService.getPerson(1).getFollowing().size());
+
     }
 
     @Test
     void addAddress() {
+
+        assertEquals(0, personService.getPerson(0).getAddresses().size());
+        personService.addAddress(0, new Address(0, "test", 12345, "test", "test", "test"));
+        assertEquals(1, personService.getPerson(0).getAddresses().size());
+
     }
 
     @Test
     void removeAddress() {
+
+        assertEquals(1, personService.getPerson(1).getAddresses().size());
+        personService.removeAddress(1, personService.getPerson(1).getAddresses().get(0));
+        assertEquals(0, personService.getPerson(1).getAddresses().size());
     }
 
     @Test
     void removePhoneNumber() {
+        assertEquals(1, personService.getPerson(1).getPhoneNumbers().size());
+        personService.removePhoneNumber(1, personService.getPerson(1).getPhoneNumbers().get(0));
+        assertEquals(0, personService.getPerson(1).getPhoneNumbers().size());
+
     }
 
     @Test
     void addPhoneNumber() {
+
+        assertEquals(0, personService.getPerson(0).getPhoneNumbers().size());
+        personService.addPhoneNumber(0, new PhoneNumber(0, "0123456789", false));
+        assertEquals(1, personService.getPerson(0).getPhoneNumbers().size());
     }
 }
--- adressverwaltung_logic/src/test/java/de/dhbw/ase/service/mocks/PersonDAOMock.java
+++ adressverwaltung_logic/src/test/java/de/dhbw/ase/service/mocks/PersonDAOMock.java
@@ -12,17 +12,26 @@ import java.util.Optional;
 public class PersonDAOMock extends PersonDAOImpl {
 
 
+
+
     private Person person = new Person(0, "vorname", "nachname", 29, 4, 1990, null, null);
 
     private Person person2 = new Person(1, "vorname2", "nachname2", 29, 4, 1990, null, null);
 
     private Person person3 = new Person(2, "vorname3", "nachname3", 29, 4, 1990, null, null);
 
+    private Address address = new Address(0, "test", 0, "test", "test", "test");
+
+    private PhoneNumber phoneNumber = new PhoneNumber(0, "0123456789", false);
+
     private List<Person> persons = new ArrayList<>();
 
 
     public PersonDAOMock() {
         persons.add(person);
+        person2.addFollowing(person);
+        person2.addAddress(address);
+        person2.addPhoneNumber(phoneNumber);
         persons.add(person2);
         persons.add(person3);
     }
